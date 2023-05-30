26fa7c42f30a6b1a474efc521f7fdd6aff2e9cf5
erkinboy-botirov <ebotirovsr@gmail.com>
2023-05-30 20:30:03 +0900

    paired dataset

--- dataset.py
+++ dataset.py
@@ -4,7 +4,7 @@ from PIL import Image
 from random import randrange
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose
-from typing import List
+from typing import Any, List, Optional, Tuple
 
 import numpy as np
 
@@ -15,18 +15,41 @@ class Modality(Enum):
     RGB = 'rgb'
     FLOW = 'flow'
 
-class Video:
+class FrameDir:
+    """Frame is a dir that stores two images: left.ext, right.ext (ext: jpg, png, tif, tiff)"""
+    path: Path
+    left_img_path: Optional[Path]
+    right_img_path: Optional[Path]
+
+    def __init__(self, frame_path: Path):
+        self.path = frame_path
+        # check if left and right images exists in the frame dir
+        # check for multiple extensions
+        for ext in ALLOWED_IMAGE_EXTENSIONS:
+            left_img_path = self.path.joinpath(f"left.{ext}")
+            right_img_path = self.path.joinpath(f"right.{ext}")
+            if left_img_path.exists() and right_img_path.exists():
+                self.left_img_path = left_img_path
+                self.right_img_path = right_img_path
+        else:
+            self.left_img_path = None
+            self.right_img_path = None
+
+    def is_valid(self):
+        return self.left_img_path is not None and self.right_img_path is not None
+    
+class VideoDir:
     """ 
-    Video in this case is a directory that stores its frames/images. 
-    Frames should be named in alphabetical order to keep the order, 
-    i.e img_001=frames[0], img_001=frames[1], img_002=frames[2], ...)
+    Video in this case is a directory that stores its frames as a dir (frame_001 > left.png, right.png). 
+    Frames should be named in alphabetical order to keep the order
     """
     path: Path
-    frames: List[Path]
+    frames: Tuple[FrameDir]
 
     def __init__(self, video_path: Path):
         self.path = video_path
-        self.frames = tuple(x for x in sorted(self.path.iterdir()) if x.suffix in ALLOWED_IMAGE_EXTENSIONS)
+        self.frames = tuple(frame for x in sorted(self.path.iterdir()) 
+                            if (frame := FrameDir(x)).is_valid())
 
     def __getitem__(self, index: int | slice) -> Path | List[Path]:
         if isinstance(index, slice):
@@ -37,9 +60,9 @@ class Video:
     def __len__(self) -> int:
         return len(self.frames)
         
-class VideoDataset(Dataset):
+class ImgPairDataset(Dataset):
     dataset_path: Path 
-    videos: List[Video]
+    videos: List[VideoDir]
     num_groups: int
     num_samples: int
     frames_per_group: int
@@ -55,7 +78,7 @@ class VideoDataset(Dataset):
         Args:
             dataset_path (Path): the file path to the root of video folder
             num_groups (int): number of frames per data sample
-            frames_per_group (int): number of frames within one group
+            frames_per_group (int): number of frames within one group 
             modality (Modality): rgb or flow
             dense_sampling (bool): dense sampling in I3D
             transform (Compose): the transformer for preprocessing
@@ -66,7 +89,7 @@ class VideoDataset(Dataset):
         """
         self.path = dataset_path
         # load only dirs that has at least one image. Later we may change minimum number of images required
-        self.videos = [video for child in self.path.iterdir() if child.is_dir() and len(video := Video(child))]
+        self.videos = [video for child in self.path.iterdir() if child.is_dir() and len(video := VideoDir(child))]
         self.num_groups = self.num_samples = num_groups
         self.frames_per_group = self.sampling_rate = frames_per_group
         self.dense_sampling = dense_sampling
@@ -77,22 +100,24 @@ class VideoDataset(Dataset):
     def __len__(self) -> int:
         return len(self.videos)
 
-    def __getitem__(self, video_index: int):
+    def __getitem__(self, video_index: int) -> Tuple[Any, Any]:
         video = self.videos[video_index] 
 
         # frame_indices = self._get_random_frame_indices_from_each_segment(video) # TAMNet normal sampling
-        images = []
+        left_images, right_images = [], []
         for i in self._get_sample_indices(video):
             for j in range(self.num_consecutive_frames):
                 frame_index = min(i+j, len(video)-1) # next frame or last frame
-                images.extend(self._load_frames(video, frame_index))
+                left_image, right_image = self._load_pair_images(video, frame_index)
+                left_images.append(left_image)
+                right_images.append(right_image)
 
-        if self.transform is None: 
-            return images
+        if self.transform is not None: 
+            return self.transform(left_images), self.transform(right_images)
         
-        return self.transform(images)
+        return left_images, right_images
     
-    def _get_sample_indices(self, video: Video) -> np.ndarray:
+    def _get_sample_indices(self, video: VideoDir) -> np.ndarray:
         max_frame_index = max(0, len(video) - self.num_consecutive_frames)
         
         if self.dense_sampling:
@@ -131,14 +156,18 @@ class VideoDataset(Dataset):
         
         return np.sort(indices)
 
-    def _get_random_frame_indices_from_each_segment(self, video: Video) -> List[int]:
+    def _get_random_frame_indices_from_each_segment(self, video: VideoDir) -> List[int]:
         segment_length = len(video) // self.num_segments
         # for i in range()
         return [randrange(i, i + segment_length) for i in range(self.num_segments)]
     
-    def _load_frames(self, video: Video, index: int) -> List[Image.Image]:
-        image = Image.open(video.frames[index]).convert('RGB')
-        image_copy = image.copy()
+    def _load_pair_images(self, video: VideoDir, index: int) -> Tuple[Image.Image, Image.Image]:
+        image = Image.open(video.frames[index].left_img_path).convert('RGB')
+        left_image = image.copy()
         image.close()
 
-        return [image_copy]
+        image = Image.open(video.frames[index].right_img_path).convert('RGB')
+        right_image = image.copy()
+        image.close()
+
+        return left_image, right_image
